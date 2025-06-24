6fb478037aac249f7a9118f1c752ee913e168652
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-06-24 10:41:02 +0300

    Use router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RootLayout } from './pages/RootLayout.tsx';
+import { IndexPage } from './pages/IndexPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <IndexPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,7 @@
+export const IndexPage = () => {
+  return (
+    <section>
+      <h1>Hello world!</h1>
+    </section>
+  );
+};
